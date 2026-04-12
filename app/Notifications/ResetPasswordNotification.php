0166d4d838ec8f0ef6baefa79b6e95503d99b06f
dipta117 <diptachy117@gmail.com>
2026-04-12 20:15:58 +0600

    feat(reset-Password): Added ResetPasswordNotification.php

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends ResetPassword
+{
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Reset Your Password - My Doctor')
+            ->greeting('Hello ' . $notifiable->name . '!')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', url('reset-password/' . $this->token . '?email=' . urlencode($notifiable->email)))
+            ->line('This password reset link will expire in ' . config('auth.passwords.users.expire', 60) . ' minutes.')
+            ->line('If you did not request a password reset, no further action is required.')
+            ->salutation('Best regards,<br>The My Doctor Team');
+    }
+}
